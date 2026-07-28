52e7ea06c4337d3a3170e288667be10f9e6fb0b4
heeju3914-dotcom <HEEJU3914@gmail.com>
2026-07-28 17:17:44 +0900

    Initial PRAPY website

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        header.classList.add("active");
+
+    }else{
+
+        header.classList.remove("active");
+
+    }
+
+});
